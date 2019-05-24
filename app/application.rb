a4b8b9420a6debc15a46ea595686305e4389379c
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-24 20:02:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|el| el.name == item_name}
       
-      if @@
+      if @@items.include?()
         
       end 
     else
